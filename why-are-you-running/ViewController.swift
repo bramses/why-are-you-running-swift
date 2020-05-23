12aa8aae7b409347c8ec9b1feef26862919a5264
Bram Adams <3282661+bramses@users.noreply.github.com>
2020-05-23 18:38:07 -0400

    Initial Commit

---
+++ why-are-you-running/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  why-are-you-running
+//
+//  Created by Macbook Pro on 5/23/20.
+//  Copyright © 2020 Bram. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
